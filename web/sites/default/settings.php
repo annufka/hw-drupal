eed296ba48c051def112329e0be1d21350783594
didukh_a <annufka94@gmail.com>
2022-04-22 22:37:46 +0300

    types

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -779,5 +779,6 @@ $databases['default']['default'] = array (
   'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
   'driver' => 'mysql',
 );
-$settings['config_sync_directory'] = 'config/sync';
+$settings['config_sync_directory'] = './config/sync';
+
 
